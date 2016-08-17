99668f5bcd7fe8787e8414c3d8eec38a7e01cadd
jingbolee <jingbolee@foxmail.com>
2016-08-17 10:39:49 +0800

    添加工具类 ImageLoader.java

---
+++ app/src/main/java/cc/lijingbo/zhihudemo/utils/ImageLoader.java
@@ -0,0 +1,9 @@
+package cc.lijingbo.zhihudemo.utils;
+
+/**
+ * @Author: Li Jingbo
+ * @Date: 2016-08-17 10:38
+ */
+public class ImageLoader {
+    private static final String TAG = "ImageLoader";
+}
